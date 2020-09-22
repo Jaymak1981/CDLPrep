a6212966c12d0135fbfd9fba20919a8dfca6e5ab
cwalbanese <631gti@gmail.com>
2020-09-22 15:59:20 -0400

    initialized firebase into app

---
+++ cdlprep/src/firebaseConfig.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: 'AIzaSyB4Kwsa1dTEIg3RdXvPM49auOag5yfzVPU',
+  authDomain: 'cdl-prep.firebaseapp.com',
+  databaseURL: 'https://cdl-prep.firebaseio.com',
+  projectId: 'cdl-prep',
+  storageBucket: 'cdl-prep.appspot.com',
+  messagingSenderId: '750711258079',
+  appId: '1:750711258079:web:3252b214591e907589a26a',
+  measurementId: 'G-Y6KV5VFZY6',
+};
+
+export default firebaseConfig;
