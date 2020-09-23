d5761f22e40f2e4afec7b332b04b18067ffa569b
cwalbanese <631gti@gmail.com>
2020-09-23 14:13:43 -0400

    added login with email

--- cdlprep/src/App.js
+++ cdlprep/src/App.js
@@ -5,6 +5,7 @@ import withFirebaseAuth from 'react-with-firebase-auth';
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import firebaseConfig from './firebaseConfig';
+import { render } from '@testing-library/react';
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const firebaseAppAuth = firebaseApp.auth();
@@ -12,21 +13,75 @@ const providers = {
   googleProvider: new firebase.auth.GoogleAuthProvider(),
 };
 
-function App(props) {
-  const { user, signOut, signInWithGoogle } = props;
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {user ? <p>Hello, {user.displayName}</p> : <p>Please sign in.</p>}
-        {user ? (
-          <button onClick={signOut}>Sign out</button>
-        ) : (
-          <button onClick={signInWithGoogle}>Sign in with Google</button>
-        )}
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { email: '', password: '' };
+  }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    console.log(this.props.user);
+    return (
+      <div className="App">
+        <header className="App-header">
+          {this.props.user ? (
+            <p>Hello, {this.props.user.displayName || this.props.user.email}</p>
+          ) : (
+            <p>Please sign in.</p>
+          )}
+          {this.props.user ? (
+            <button onClick={this.props.signOut}>Sign out</button>
+          ) : (
+            <div>
+              <input
+                type="text"
+                placeholder="Email"
+                name="email"
+                onChange={this.handleChange}
+              ></input>
+              <input
+                type="text"
+                name="password"
+                placeholder="Password"
+                onChange={this.handleChange}
+              ></input>
+              <div>
+                {' '}
+                <button
+                  onClick={() =>
+                    this.props.signInWithEmailAndPassword(
+                      this.state.email,
+                      this.state.password
+                    )
+                  }
+                >
+                  Sign in
+                </button>
+                <button
+                  onClick={() =>
+                    this.props.createUserWithEmailAndPassword(
+                      this.state.email,
+                      this.state.password
+                    )
+                  }
+                >
+                  Sign up
+                </button>
+              </div>
+
+              <button onClick={this.props.signInWithGoogle}>
+                Sign in with Google
+              </button>
+            </div>
+          )}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default withFirebaseAuth({
