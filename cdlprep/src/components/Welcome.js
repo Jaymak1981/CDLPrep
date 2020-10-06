41f59bd5498accb2cc02625b534d3be52c1322e8
Jaymak1981 <jaymak1231@gmail.com>
2020-10-06 12:57:36 -0700

    added content

--- cdlprep/src/components/Welcome.js
+++ cdlprep/src/components/Welcome.js
@@ -1,59 +1,47 @@
 import React, { Component } from 'react';
 
 class Welcome extends Component {
-  render() {
-    return (
-      <div>
-        <p>
-          <strong>First Time CDL Applicatnts</strong>
-        </p>
-        <p>
-          Welcome to the school of CDL Prep! Here you can find all your testing
-          needs to get ready for the CDL test.
-        </p>
-        <p>
-          Welcome to the school of CDL Prep! Here you can find all your testing
-          needs to get ready for the CDL test.
-        </p>
-        <p>
-          Welcome to the school of CDL Prep! Here you can find all your testing
-          needs to get ready for the CDL test.
-        </p>
-        <p>
-          Welcome to the school of CDL Prep! Here you can find all your testing
-          needs to get ready for the CDL test.
-        </p>
-        <p>
-          Welcome to the school of CDL Prep! Here you can find all your testing
-          needs to get ready for the CDL test.
-        </p>
-        <p>
-          Welcome to the school of CDL Prep! Here you can find all your testing
-          needs to get ready for the CDL test.
-        </p>
-        <p>
-          Welcome to the school of CDL Prep! Here you can find all your testing
-          needs to get ready for the CDL test.
-        </p>
-        <p>
-          Welcome to the school of CDL Prep! Here you can find all your testing
-          needs to get ready for the CDL test.
-        </p>
-        <p>
-          Welcome to the school of CDL Prep! Here you can find all your testing
-          needs to get ready for the CDL test.
-        </p>
-        <p>
-          Welcome to the school of CDL Prep! Here you can find all your testing
-          needs to get ready for the CDL test.
-        </p>
-        <p>
-          Welcome to the school of CDL Prep! Here you can find all your testing
-          needs to get ready for the CDL test.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return ( <div >
+            <p>
+            <strong > First Time CDL Applicatnts </strong> 
+            </p> 
+            <p>
+            You must first obtain a CDL Learner Permit to operate a vehicle requiring a CDL on the Highway.The Learners Permit is valid
+            for 6 months.The minimum age to aquire a CDL Learners Permit is 21, 18
+            if you are an intrastate commercial driver valid only in the state of your home residence.All drivers under the age of 21 are prohibited form transporting Hazardous Material.
+            </p> 
+            
+            <p>
+             Before a Commercial Learners Permit is issued, you are required to successfully complete Knowledge tests for the class and type of vehicle you plan to start driving.
+
+             You must have the following documents before you take any tests:
+             1- A valid D.O.T. Medical Certificate
+             2- A valid Social Security Card
+             3- A valid operators license
+             4- Proof of Status
+             5- Proof of identification and age
+             6- Proof of Residence
+             7- Proof of one year of driving experience 
+            </p> 
+            
+            <p>
+            Welcome to the school of CDL Prep!Here you can find all your testing needs to get ready
+            for the CDL test. 
+            </p> 
+            
+            <p>
+            Welcome to the school of CDL Prep!Here you can find all your testing needs to get ready
+            for the CDL test. 
+            </p> 
+            
+            <p>
+            Welcome to the school of CDL Prep!Here you can find all your testing needs to get ready
+            for the CDL test. 
+            </p> 
+            </div>
+        );
+    }
 }
 
 export default Welcome;
